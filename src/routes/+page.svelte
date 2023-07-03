39bc3de604c599db48799e057f462eafda9db11e
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-07-03 20:47:14 -0300

    🚀 chore: initial project setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import '~/app.css';
+</script>
+
+<div class="w-full h-full flex justify-center items-center">
+  <h1 class="text-2xl">Whitepaper</h1>
+</div>
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'~': path.resolve(__dirname, './src')
+		}
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
